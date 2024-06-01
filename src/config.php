01b57137a3bf61ceca20680f3bbd527ccec27dc1
Maxxavec2x <maxencesibonibergard@gmail.com>
2024-06-01 11:20:23 +0200

    modification de la fonction getUSerByID, ajout d'un import de config.php en haut du fichier pour pouvoir accéder à , et changement de la page cible du bouton de login dans banner.php

--- src/config.php
+++ src/config.php
@@ -12,8 +12,19 @@ define('DB_USER', 'root');
 define('DB_PASS', 'rootpassword');
 
 // connect to database
+//print(DB_HOST);
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-#var_dump($conn);
+// var_dump($conn);
+
+
+// $result = mysqli_query($conn, "SHOW TABLES");
+// if ($result) {
+//     while ($row = mysqli_fetch_array($result)) {
+//         echo $row[0] . "<br>";
+//     }
+// } else {
+//     echo "Erreur lors de la récupération des tables : " . mysqli_error($conn);
+// }
 
 //define some constants:
 define('ROOT_PATH', realpath(dirname(__FILE__)));
--- src/includes/public/registration_login.php
+++ src/includes/public/registration_login.php
@@ -1,4 +1,5 @@
 <?php
+include('../../config.php');
 // variable declaration
 $username = "";
 $email = "";
@@ -15,6 +16,7 @@ if (isset($_POST['login_btn'])) {
     }
     if (empty($errors)) {
         $password = md5($password); // encrypt password
+        //var_dump($password);
         $sql = "SELECT * FROM users WHERE username='$username' and password='$password' LIMIT 1";
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) > 0) {
@@ -23,14 +25,17 @@ if (isset($_POST['login_btn'])) {
             //var_dump(getUserById($reg_user_id)); die();
             // put logged in user into session array
             $_SESSION['user'] = getUserById($reg_user_id);
+            //var_dump($_SESSION['user'] );
             // if user is admin, redirect to admin area
             if (in_array($_SESSION['user']['role'], ["Admin", "Author"])) {
                 $_SESSION['message'] = "You are now logged in";
+                //print("C BON TU EST CONNECTE EN ADMIN \n");
                 // redirect to admin area
                 header('location: ' . BASE_URL . '/admin/dashboard.php');
                 exit(0);
             } else {
                 $_SESSION['message'] = "You are now logged in";
+               // print("C BON TU EST CONNECTE EN USER \n");
                 // redirect to public area
                 header('location: index.php');
                 exit(0);
@@ -41,4 +46,25 @@ if (isset($_POST['login_btn'])) {
     }
 }
 
+
+// Get user info from user id
+function getUserById($id)
+{
+    global $conn; // rendre disponible, à cette fonction, la variable de connexion $conn
+    $sql ="SELECT username, role FROM users WHERE id = " . $id . " LIMIT 1;"; // requête qui récupère le user et son rôle
+    $result = mysqli_query($conn, $sql); // la fonction php-mysql
+    $user = mysqli_fetch_assoc($result); // mettre $result au format associatif pour un seul résultat
+    return $user;
+}
+// escape value from form
+function esc(String $value)
+{
+    // bring the global db connect object into function
+    global $conn;
+    $val = trim($value); // remove empty space sorrounding string
+    $val = mysqli_real_escape_string($conn, $value);
+    return $val;
+
+
+}
 
