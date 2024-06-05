fe4b2bcf74c9e336a9510a845d35080488226ecc
Maxxavec2x <maxencesibonibergard@gmail.com>
2024-06-05 14:26:45 +0200

    Amélioration de l'affichage des posts pour tenir compte des exigences de l'E8

--- src/index.php
+++ src/index.php
@@ -30,8 +30,13 @@ include(ROOT_PATH . '/includes/all_functions.php');
 			<h2 class="content-title">Recent Articles</h2>
 			<hr>
 			<?php foreach(getPublishedPosts() as $post){ ?>
-				<p><?php echo $post['title'];?></p>
-				<p><?php echo $post['topic'];?></p>
+				<div class="post" style="margin-left: 0px;">
+					<p>Titre : <?php echo $post['title'];?></p>
+					<img src="static/images/<?php echo 'post_image' . $post['id'] . '.jpg';?>" alt="Alternative" width="100%" width="100%">
+					<p>Topic : <?php echo $post['topic'];?></p>
+					<p>Date de publication de l'article : <?php echo $post['updated_at'];?></p> <!--  En vrai hors vanne je sais pas si la date de publication c'est updated_at ou created_at, donc à voir. -->
+					<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>"> Read more </a>
+				</div>
 			<?php } ?>
 		</div>
 		<!-- // content -->
