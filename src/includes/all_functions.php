2093e62d607b59591c06120955074b21cf2041f9
Maxxavec2x <maxencesibonibergard@gmail.com>
2024-06-05 12:39:29 +0200

    création de la fonction getPostTopic()

--- src/includes/all_functions.php
+++ src/includes/all_functions.php
@@ -5,4 +5,17 @@
         $result = mysqli_query($conn, $sql);
         return mysqli_fetch_all($result, MYSQLI_ASSOC); // tableau associatif des posts publiés
     }
+
+    function getPostTopic($post_id) {
+        global $conn;
+        $sql = "SELECT * FROM topics WHERE id=(SELECT topic_id from post_topic WHERE post_id=$post_id;)";
+        $topic = mysqli_query($conn, $sql);
+        return $topic;
+    }
+
+
+
+
+
+
 ?>
--- src/login.php
+++ src/login.php
@@ -1,23 +1,17 @@
-<?php include('config.php'); ?>
-
-<?php include('includes/public/registration_login.php'); ?>
-
-
-
-<?php include('includes/public/head_section.php'); ?>
+<?php
+include('config.php');
+include('includes/public/registration_login.php');
+include('includes/public/head_section.php');
+?>
 
 <title>MyWebSite | Sign in </title>
 </head>
 
 <body>
-
 	<div class="container">
-
 		<!-- Navbar -->
 		<?php include(ROOT_PATH . '/includes/public/navbar.php'); ?>
 		<!-- // Navbar -->
-
-
 		<div style="width: 40%; margin: 20px auto;">
 			<form method="post" action="login.php">
 				<h2>Login</h2>
@@ -32,14 +26,10 @@
 				</p>
 			</form>
 		</div>
-
-
 	</div>
 	<!-- // content -->
-
 	</div>
 	<!-- // container -->
-
 	<!-- Footer -->
 	<?php include(ROOT_PATH . '/includes/public/footer.php'); ?>
 	<!-- // Footer -->
