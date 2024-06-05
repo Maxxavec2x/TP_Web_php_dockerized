82660a01f045a0abedd794a25c9bfce1229ec6e0
Maxxavec2x <maxencesibonibergard@gmail.com>
2024-06-05 12:07:34 +0200

    Changement de l'affichage de errors.php pour 'Respecter le style général du site'

--- src/includes/public/errors.php
+++ src/includes/public/errors.php
@@ -1,7 +1,9 @@
 <p>
-    <?php
-        if(count($errors)){
-            var_dump($errors);
-        }
-    ?>
+<?php if (count($errors) > 0): ?>
+    <div class="validation_errors">
+        <?php foreach ($errors as $error): ?>
+            <p class="error"><?php echo $error; ?></p>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
 </p>
