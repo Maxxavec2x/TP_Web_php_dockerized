1e753656751c7f7717bb6cb535842301b47a4c1a
Maxxavec2x <maxencesibonibergard@gmail.com>
2024-06-07 13:51:16 +0200

    Développement de la page single_post.php, ajout des fonctions gePost() et getAllTopics() dans all_fonctions.php. Amélioration de la fonction getTopics() (utilisation de mysqli_fetch_assoc à la place de mysqli_fetch_all)

--- src/includes/all_functions.php
+++ src/includes/all_functions.php
@@ -5,7 +5,7 @@
         $result = mysqli_query($conn, $sql);
         $publishedPosts = mysqli_fetch_all($result, MYSQLI_ASSOC); // tableau associatif des posts publiés
         foreach ($publishedPosts as &$post) {
-            $post['topic'] = getPostTopic($post['id'])[0]['name'];
+            $post['topic'] = getPostTopic($post['id'])['name'];
         }
         return $publishedPosts;
     }
@@ -14,7 +14,24 @@
         global $conn;
         $sql = "SELECT * FROM topics WHERE id=(SELECT topic_id from post_topic WHERE post_id=$post_id);";
         $topic = mysqli_query($conn, $sql);
-        return mysqli_fetch_all($topic, MYSQLI_ASSOC);;
+        return mysqli_fetch_assoc($topic);;
     }
+
+    function getPost($slug) {
+        global $conn;
+        $sql = "SELECT * FROM posts WHERE slug='$slug';";
+        $result = mysqli_query($conn, $sql);
+        $post = mysqli_fetch_assoc($result);
+        $post['topic'] = getPostTopic($post['id'])['name'];
+        return $post;
+}
+
+function getAllTopics() {
+    global $conn;
+    $sql="SELECT * FROM topics;";
+    $result = mysqli_query($conn, $sql);
+    $topics = mysqli_fetch_all($result, MYSQLI_ASSOC); // tableau associatif des posts publiés
+    return $topics ;
+}
 
 ?>
--- src/single_post.php
+++ src/single_post.php
@@ -1,1 +1,53 @@
+<?php
+include('config.php');
+include('includes/public/head_section.php');
+include(ROOT_PATH . '/includes/all_functions.php');
+$postSlug = $_GET['post-slug']; //je pose ça là au cas où
+$post = getPost($postSlug);
+?>
 
+<title> <?php echo $post['title'] ?> | MyWebSite</title>
+</head>
+
+<body>
+    <div class="container">
+        <!-- Navbar -->
+        <?php include(ROOT_PATH . '/includes/public/navbar.php'); ?>
+        <!-- // Navbar -->
+        <div class="content">
+            <!-- Page wrapper -->
+            <div class="post-wrapper">
+                <!-- full post div -->
+                <div class="full-post-div">
+                    <h2 class="post-title"> <?php echo $post['title'] ?> </h2>
+                        <div class="post-body-div">
+                            <?php echo $post['body'] ?>
+                        </div>
+                </div>
+                <!-- // full post div -->
+            </div>
+            <!-- // Page wrapper -->
+            <!-- post sidebar -->
+            <div class="post-sidebar">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Topics</h2>
+                    </div>
+                    <div class="card-content">
+                        <?php foreach(getAllTopics() as $topic) {  ?>
+                            <br>
+                            <a href="filtered_posts.php"> <h3> <?php  echo $topic['name']; ?> </h3> </a>
+                        <?php } ?>
+                        <br>
+                    </div>
+                </div>
+            </div>
+            <!-- // post sidebar -->
+        </div>
+    </div>
+    <!-- // content -->
+
+
+    <!-- Footer -->
+    <?php include(ROOT_PATH . '/includes/public/footer.php'); ?>
+    <!-- // Footer -->
