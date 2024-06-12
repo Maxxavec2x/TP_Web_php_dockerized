3a0ec3b6e2642ea87b6f6ce52ccf03f9a2cddd7c
Maxxavec2x <maxence.siboni-bergard@insa-cvl.fr>
2024-06-12 11:27:42 +0200

    ajout de la page posts.php pour manage les posts

--- src/admin/post_functions.php
+++ src/admin/post_functions.php
@@ -1,1 +1,79 @@
+<?php
 
+/*--------------------
+post_functions.php
+------------------------*/
+
+$post_id = 0;
+$isEditingPost = false;
+$published = 0;
+$title = "";
+$post_slug = "";
+$body = "";
+$featured_image = "";
+$post_topic = "";
+
+/* - - - - - - - - - -
+- Post functions
+- - - - - - - - - - -*/
+
+// get all posts from WEBLOG DATABASE
+function getAllPosts() {
+    global $conn;
+    $sql = "SELECT * FROM posts";
+    $result = mysqli_query($conn, $sql);
+    $posts = mysqli_fetch_all($result, MYSQLI_ASSOC); // tableau associatif des posts publiés
+    foreach ($posts as &$post) {
+        $post['author'] = getPostAuthorById($post['id']);
+    }
+   // var_dump($posts);
+    return $posts;
+}
+
+function createPost($request_values) {
+    global $conn, $errors, $title, $featured_image, $topic_id, $body, $published;
+}
+
+// get the author/username of a post
+function getPostAuthorById($user_id) {
+    global $conn;
+    $sql = "SELECT username FROM users WHERE id=$user_id";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        // return username
+        return mysqli_fetch_assoc($result)['username'];
+    } else {
+        return null;
+    }
+}
+
+function editPost($role_id) {
+    global $conn, $title, $post_slug, $body, $isEditingPost, $post_id;
+}
+
+function updatePost($request_values) {
+    global $conn, $errors, $post_id, $title, $featured_image, $topic_id, $body, $published;
+}
+
+// delete blog post
+function deletePost($post_id) {
+    global $conn;
+    $sql = "DELETE FROM posts WHERE id=$post_id";
+    if (mysqli_query($conn, $sql)) {
+        $_SESSION['message'] = "Post successfully deleted";
+        header("location: posts.php");
+        exit(0);
+    }
+}
+
+// toggle blog post: published↔unpublished
+function togglePublishPost($post_id, $message) {
+    global $conn;
+    $sql = "UPDATE posts SET published=!published WHERE id=$post_id";
+    if (mysqli_query($conn, $sql)) {
+        $_SESSION['message'] = $message;
+        header("location: posts.php");
+        exit(0);
+    }
+}
+?>
--- src/admin/posts.php
+++ src/admin/posts.php
@@ -1,1 +1,88 @@
+<?php include('../config.php'); ?>
+<?php include(ROOT_PATH . '/includes/admin_functions.php'); ?>
+<?php include(ROOT_PATH . '/includes/admin/head_section.php'); ?>
+<?php include('post_functions.php'); ?>
+<!-- //BTW: ideally we need to create a role_user table (users<->role_user<->roles)
+	// role_user(id, user_id,role_id)
+
+	^ C'est le prof qui a écrit ça btw, j'ai créé la table mais j'ai pas compris l'intérêt dans les fonctions getAdminRoles() et getAdminUsers() 🤔
+	Je pense que c'est bcp plus simple de query role et user directement. J'imagine que l'intérêt c'est de pouvoir créer des users avec plusieurs roles ?
+
+	-->
+<?php
+// Get all admin roles from DB : by admin roles i mean (Admin or Author)
+$roles = getAdminRoles(); // table roles
+
+// Get all admin users from DB
+$admins = getAdminUsers(); // by admin roles i mean (Admin or Author), table users
+?>
+
+<title>Admin | Manage users</title>
+</head>
+
+<body>
+	<!-- admin navbar -->
+	<?php include(ROOT_PATH . '/includes/admin/header.php') ?>
+	<div class="container content">
+		<!-- Left side menu -->
+		<?php include(ROOT_PATH . '/includes/admin/menu.php') ?>
+
+		<!-- Display records from DB-->
+<div class="table-div">
+
+<!-- Display notification message -->
+<?php include(ROOT_PATH . '/includes/public/messages.php') ?>
+<?php $posts = getAllPosts(); ?>
+
+    <table class="table">
+        <thead>
+            <th>N</th>
+            <th>Author</th>
+            <th>Title</th>
+            <th>Views</th>
+            <th>Publish</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </thead>
+        <tbody>
+            <?php foreach ($posts as $key => $post) : ?>
+                <tr>
+                    <td><?php echo $key + 1; ?></td>
+                    <td>
+                        <?php echo $post['author']; ?>, &nbsp;
+                    </td>
+                    <td><?php echo $post['title']; ?></td>
+                    <td><?php echo $post['views']; ?></td>
+                    <td>
+                       
+                        <a class="fa fa-pencil btn unpublish" href="posts.php?unpublish=<?php echo $post['id'] ?>">
+                        </a>
+                    </td>
+                    <td>
+                        <a class="fa fa-pencil btn edit" href="create_post.php?edit-post=<?php echo $post['id'] ?>">
+                        </a>
+                    </td>
+                    <td>
+                        <a class="fa fa-trash btn delete" href="create_post.php?delete-post=<?php echo $post['id'] ?>">
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+</div>
+</div>
+<!-- // Display records from DB -->
+
+	</div>
+
+</body>
+
+</html>
+
+
+
+
+
+
 
